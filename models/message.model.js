7de1beafd0bf1264fa58ba17cfb8e48f4688dab5
Soumahoro-Aboubakar <aboubakarkone8958@gmail.com>
2025-06-29 20:10:11 +0000

    29/06/2025

--- models/message.model.js
+++ models/message.model.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
+    id:{type : String, required : true}, //très important pour mettre à jours le status du message côté frontend
     sender: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     receiver: { type: Schema.Types.ObjectId, ref: 'User' }, // Pour messages privés
     room: { type: Schema.Types.ObjectId, ref: 'Room' }, // Pour messages de groupe
@@ -11,8 +12,8 @@ const MessageSchema = new Schema({
     isAnonymous: Boolean,
     status: {
         type: String,
-        enum: ['SENT', 'DELIVERED',  'READ', 'FAILED' , "PENDING"], 
-        default: 'SENT'
+        enum: ['SENT', 'DELIVERED',  'READ', 'FAILED' , 'RECEIVED_BY_SERVER' ,"PENDING"], 
+        default: 'RECEIVED_BY_SERVER'
     },
     isArchived: { type: Boolean, default: false },
     seenBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
