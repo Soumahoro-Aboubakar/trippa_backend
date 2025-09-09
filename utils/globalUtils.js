f5a42f2c1551580401d778c4701e9280b8539446
Soumahoro-Aboubakar <aboubakarkone8958@gmail.com>
2025-09-09 01:25:32 +0000

    version 2

--- utils/globalUtils.js
+++ utils/globalUtils.js
@@ -1,3 +1,17 @@
 
+import { ObjectId } from "mongodb";
+
 export const minimumViewThresholdDefault = 100;;
 export const pricePerViewDefault = 0.001;
+
+export const  normalizeUserId = (userId) => {
+  if (userId instanceof ObjectId) {
+    return userId; // déjà un ObjectId
+  }
+
+  if (typeof userId === "string" && ObjectId.isValid(userId)) {
+    return new ObjectId(userId); // conversion depuis string
+  }
+
+  throw new Error("L'userId fourni n'est pas valide.");
+}
